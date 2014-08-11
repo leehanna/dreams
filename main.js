b76f326def78f2493e70861428b231d53f3e3b73
leehanna <hannallee@berkeley.edu>
2014-08-11 11:32:56 -0700

    added panda

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
 $(document).ready(function() {
 	$('#maintitle').textillate({initialDelay:1500, in: {effect: 'fadeIn', delay: 100, sync: false, shuffle: true}});
 
-	function onYouTubePlayerReady(playerId) {
-		var player =  iframe.getElementById('player');
-		player.mute();
+	var div = document.getElementsByClassName("bgcontainer");
+	var speed = 1.5;
+
+	window.onscroll = function()
+	{
+		var yOffset = window.pageYOffset;
+		div.style.backgroundPosition = "0px " + (yOffset/speed) + "px";
 	}
-
-	// var div = document.getElementsByClassName("bgcontainer");
-	// var speed = 1.5;
-
-	// window.onscroll = function()
-	// {
-	// 	var yOffset = window.pageYOffset;
-	// 	div.style.backgroundPosition = "0px " + (yOffset/speed) + "px";
-	// }
 });
