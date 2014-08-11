cb5b9a10fd7d776fdc81c6016c1e32a6de3b2f97
leehanna <hannallee@berkeley.edu>
2014-08-11 01:16:10 -0700

    background fix, unnecessary images removed

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
 	$('#maintitle').textillate({initialDelay:1500, in: {effect: 'fadeIn', delay: 100, sync: false, shuffle: true}});
+
+	var div = document.getElementsByClassName("bgcontainer");
+	var speed = 1.5;
+
+	window.onscroll = function()
+	{
+		var yOffset = window.pageYOffset;
+		div.style.backgroundPosition = "0px " + (yOffset/speed) + "px";
+	}
 });
