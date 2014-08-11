6154412975b95d99829c4f2304977c858bc5ef4e
leehanna <hannallee@berkeley.edu>
2014-08-11 02:49:16 -0700

    video as fixed bg

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
 	$('#maintitle').textillate({initialDelay:1500, in: {effect: 'fadeIn', delay: 100, sync: false, shuffle: true}});
 
+	function onYouTubePlayerReady(playerId) {
+		var player =  iframe.getElementById('player');
+		player.mute();
+	}
+
 	// var div = document.getElementsByClassName("bgcontainer");
 	// var speed = 1.5;
 
