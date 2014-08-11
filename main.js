7f48babfb185982c7a17ed4b1c6fd29af9a8f996
leehanna <hannallee@berkeley.edu>
2014-08-11 15:58:12 -0700

    full width video

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
 $(document).ready(function() {
 	$('#maintitle').textillate({initialDelay:1500, in: {effect: 'fadeIn', delay: 100, sync: false, shuffle: true}});
-
-	var div = document.getElementsByClassName("bgcontainer");
-	var speed = 1.5;
-
-	window.onscroll = function()
-	{
-		var yOffset = window.pageYOffset;
-		div.style.backgroundPosition = "0px " + (yOffset/speed) + "px";
-	}
 });
